b8464f4f8691e97ce3ba8cd8f551f441fbd6fe41
帅凯铭 帅凯铭 <skm1553408537@outlook.com>
2021-01-05 12:06:11 +0800

    shuai

--- BLL/ShuaiBLL/BaoSunManager.cs
+++ BLL/ShuaiBLL/BaoSunManager.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL;
 
-namespace DAL.ShuaiDAL
+namespace BLL.ShuaiBLL
 {
-    class Class1
+   public  class BaoSunManager
     {
-    }
+       
+        }
 }
