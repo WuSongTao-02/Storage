19c5586b69b907a01a251b7653c72b0c33e0af55
Can53 <2389375185@qq.com>
2021-01-05 09:54:19 +0800

    9.54test

--- BLL/HuangBLL/AdminManager.cs
+++ BLL/HuangBLL/AdminManager.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BLL.HuangBLL
 {
-    class Class1
+    class AdminManager
     {
     }
 }
--- DAL/HuangDAL/AdminService.cs
+++ DAL/HuangDAL/AdminService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DAL.HuangDAL
 {
-    class Class1
+    class AdminService
     {
     }
 }
