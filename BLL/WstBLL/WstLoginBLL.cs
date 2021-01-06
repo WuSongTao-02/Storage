d41d0ebc15fc9af87e14e9f4a05f9d4a02174989
松涛 吴 <1696718534@qq.com>
2021-01-06 10:28:05 +0800

    2021-1-6 wst

--- BLL/WstBLL/WstLoginBLL.cs
+++ BLL/WstBLL/WstLoginBLL.cs
@@ -4,22 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Model;
+using DAL;
 
-namespace DAL.WstDAL
+namespace BLL.WstBLL
 {
-   public  class WstDAL
+    public class WstLoginBLL
     {
-       static CangChuEntities entity = new CangChuEntities();
-
         /// <summary>
         /// 登录
         /// </summary>
         /// <param name="name">用户名</param>
         /// <param name="pwd">密码</param>
         /// <returns>受影响行数</returns>
-        public static int Login(string name, string pwd) {
-            var count = (from p in entity.Admin where p.UserName == name && p.PassWord == pwd&&p.IsDelete==0&&p.RoleId==1||p.RoleId==2 select p).Count();
-            return count;
+        public static int Login(string name, string pwd)
+        {
+            return DAL.WstDAL.WstLoginDAl.Login(name, pwd);
         }
-    }
+
+        }
 }
