6cfe7bab4e48e406d6b1877868e745440a07f72e
帅凯铭 帅凯铭 <skm1553408537@outlook.com>
2021-01-08 16:15:37 +0800

    111

--- WebApplication1/Controllers/shuaikmController.cs
+++ WebApplication1/Controllers/shuaikmController.cs
@@ -15,6 +15,15 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
+        public ActionResult yiku() {
+            return View();
+        }
+        public ActionResult pandian() {
+            return View();
+        }
+        public ActionResult cate() {
+            return View();
+        }
 
         public ActionResult ChaXun() {
 
@@ -24,16 +33,27 @@ namespace WebApplication1.Controllers
         public ActionResult GetRows() {
             return Json(BLL.ShuaiBLL.BaoSunManager.GetRows(), JsonRequestBehavior.AllowGet);
 
-
+        }
+        public ActionResult GetRows1() {
+            return Json(BLL.ShuaiBLL.YiKuManager.GetRows1(),JsonRequestBehavior.AllowGet);
+        
         }
         public ActionResult edit(int id)
         {
             return Json(BLL.ShuaiBLL.BaoSunManager.edit(id),JsonRequestBehavior.AllowGet);
         }
+        public ActionResult yikuedit(int id) {
+            return Json(BLL.ShuaiBLL.YiKuManager.yikuedit(id),JsonRequestBehavior.AllowGet);
+        
+        }
             public ActionResult GetPageList(int PageIndex, int PageSize){
 
             return Json(BLL.ShuaiBLL.BaoSunManager.GetPageList(PageIndex,PageSize),JsonRequestBehavior.AllowGet);
             }
+
+        public ActionResult YikuPageList(int PageIndex, int PageSize) {
+            return Json(BLL.ShuaiBLL.YiKuManager.YikuPageList(PageIndex,PageSize),JsonRequestBehavior.AllowGet) ;
+        }
 
         public ActionResult GetPageList1(int PageIndex, int PageSize, string name) {
             return Json(BLL.ShuaiBLL.BaoSunManager.GetPageList1(PageIndex,PageSize,name),JsonRequestBehavior.AllowGet);
@@ -44,5 +64,17 @@ namespace WebApplication1.Controllers
             return Json(BLL.ShuaiBLL.BaoSunManager.ChaXun2(name),JsonRequestBehavior.AllowGet);
         
         }
+        public ActionResult Yikuchaxun() {
+            return Json(BLL.ShuaiBLL.YiKuManager.Yikuchaxun(),JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult YikuPageList1(int PageIndex, int PageSize, string name) {
+            return Json(BLL.ShuaiBLL.YiKuManager.YikuPageList1(PageIndex,PageSize,name),JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult YikuPageList2(int PageIndex,int PageSize,string name, DateTime sname, DateTime sname2) {
+            return Json(BLL.ShuaiBLL.YiKuManager.YikuPageList2(PageIndex,PageSize,name,sname,sname2),JsonRequestBehavior.AllowGet);
+        }
+
         }
 }
