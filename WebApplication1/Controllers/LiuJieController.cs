165e127dfc6bd3f0eee82da526f8d6fa114835f5
杰 刘 <2023199610@qq.com>
2021-01-08 11:00:48 +0800

    test

--- WebApplication1/Controllers/LiuJieController.cs
+++ WebApplication1/Controllers/LiuJieController.cs
@@ -16,11 +16,80 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
-        public ActionResult PageListProbaict(int pageIndex, int pageSize) {
-            return Json(ProbaictManager.PageListProbaict(pageIndex,pageSize),JsonRequestBehavior.AllowGet);
+
+        /// <summary>
+        /// 产品页面分页查询
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetPageListProbaict(int pageIndex, int pageSize)
+        {
+            return Json(ProbaictManager.PageListProbaict(pageIndex, pageSize), JsonRequestBehavior.AllowGet);
         }
-        public ActionResult GetRows() {
+
+        /// <summary>
+        /// 产品页面数据条数
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetRowsProbaict() {
             return Json(ProbaictManager.GetRows(),JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// 产品页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ChanPingYeMian() {
+            return View();
+        }
+
+        /// <summary>
+        /// 产品删除
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult GetDeleteProbaict(string id) {
+            return Json(ProbaictManager.GetDelete(id),JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// 根据id查询产品
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetByIdProbaict(string id) {
+            return Json(ProbaictManager.GetById(id),JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// 产品新增页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ChanPingYeMianAdd() {
+            return View();
+        }
+
+        /// <summary>
+        /// 产品新增
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public ActionResult GetAddProbaict(Probaict p) {
+            return Json(ProbaictManager.Add(p),JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// 产品修改页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ChanPingYeMianUpdate() {
+            return View();
+        }
+
+        /// <summary>
+        /// 产品修改
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetUpdateProbaict(Probaict p) {
+            return Json(ProbaictManager.Update(p),JsonRequestBehavior.AllowGet);
         }
     }
 }
