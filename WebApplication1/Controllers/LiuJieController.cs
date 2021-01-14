d5bbede568ecdf8132ae1c7855ae3b06cf53c97f
杰 刘 <2023199610@qq.com>
2021-01-14 19:54:58 +0800

    1414

--- WebApplication1/Controllers/LiuJieController.cs
+++ WebApplication1/Controllers/LiuJieController.cs
@@ -243,10 +243,110 @@ namespace WebApplication1.Controllers
         }
 
         /// <summary>
-        /// 出库新增页面
+        /// Warehouse表总条数
         /// </summary>
         /// <returns></returns>
-        public ActionResult ChuKuYeMianAdd()
+        public ActionResult GetRowsDeliver()
+        {
+            return Json(DeliverManager.GetRowsDeliver(), JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// 查询入库分页Warehouse所有数据
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetPageListDeliver(int pageIndex, int pageSize)
+        {
+            return Json(DeliverManager.GetPageListDeliver(pageIndex, pageSize), JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// 分页查询Warehouse表所有数据
+        /// 状态为1:待审核
+        /// </summary>
+        /// <param name="pageIndex"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        public ActionResult GetPageListDeliver1(int pageIndex, int pageSize)
+        {
+            return Json(DeliverManager.GetPageListDeliver1(pageIndex, pageSize), JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// 分页查询Warehouse表所有数据
+        /// 状态为2:审核通过
+        /// </summary>
+        /// <param name="pageIndex"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        public ActionResult GetPageListDeliver2(int pageIndex, int pageSize)
+        {
+            return Json(DeliverManager.GetPageListDeliver2(pageIndex, pageSize), JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// 分页查询Warehouse表所有数据
+        /// 状态为3:审核失败
+        /// </summary>
+        /// <param name="pageIndex"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        public ActionResult GetPageListDeliver3(int pageIndex, int pageSize)
+        {
+            return Json(DeliverManager.GetPageListDeliver3(pageIndex, pageSize), JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// 根据时间区间查询
+        /// </summary>
+        /// <param name="pageIndex"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        public ActionResult GetPageListDeliverShiJian(int pageIndex, int pageSize, DateTime date1, DateTime date2)
+        {
+            return Json(DeliverManager.GetPageListDeliverShiJian(pageIndex,pageSize,date1,date2),JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// 根据编号查询
+        /// </summary>
+        /// <param name="pageIndex"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        public ActionResult GetPageListDeliverBianHao(int pageIndex, int pageSize, string bianhao)
+        {
+            return Json(DeliverManager.GetPageListDeliverBianHao(pageIndex,pageSize,bianhao),JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// 根据编号时间两个条件进行查询
+        /// </summary>
+        /// <param name="pageIndex"></param>
+        /// <param name="pageSize"></param>
+        /// <param name="bianhao"></param>
+        /// <param name="date1"></param>
+        /// <param name="date2"></param>
+        /// <returns></returns>
+        public ActionResult GetPageListDeliverTiaoJian(int pageIndex, int pageSize, string bianhao, DateTime date1, DateTime date2)
+        {
+            return Json(DeliverManager.GetPageListDeliverTiaoJian(pageIndex,pageSize,bianhao,date1,date2),JsonRequestBehavior.AllowGet);
+        }
+
+            /// <summary>
+            /// 删除
+            /// </summary>
+            /// <param name="id"></param>
+            /// <returns></returns>
+            public ActionResult GetDeleteDeliver(int id)
+        {
+            return Json(DeliverManager.GetDeleteDeliver(id),JsonRequestBehavior.AllowGet);
+        }
+
+            /// <summary>
+            /// 出库新增页面
+            /// </summary>
+            /// <returns></returns>
+            public ActionResult ChuKuYeMianAdd()
         {
             return View();
         }
