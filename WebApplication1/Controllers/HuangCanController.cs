60fb28154c09965b4a9d74ab1c3b67226383d9f5
Can53 <2389375185@qq.com>
2021-01-17 01:04:35 +0800

    33

---
+++ WebApplication1/Controllers/HuangCanController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model;
+using Model.Huang;
+using BLL.HuangBLL;
+namespace WebApplication1.Controllers
+{
+    public class HuangCanController : Controller
+    {
+        // GET: HuangCan
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult queryDept()
+        {
+            return Json(DeptManager.queryDept(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult queryRole()
+        {
+            return Json(RoleManager.queryRole(), JsonRequestBehavior.AllowGet);
+        }
+
+
+
+
+        public ActionResult DemoPageList(int pageindex, int pagesize)
+        {
+            return Json(AdminManager.PageListDemo(pageindex, pagesize), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetRows()
+        {
+            return Json(AdminManager.GetRows(), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult dele(Admin ad)
+        {
+            return Json(AdminManager.dele(ad), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Add(Admin ad)
+        {
+
+            ad.CreateTime = DateTime.Now;
+            return Json(AdminManager.Add(ad), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Edit(Admin ad)
+        {
+            return Json(AdminManager.Edit(ad), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetById(int id)
+        {
+            return Json(AdminManager.GetById(id), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Update()
+        {
+            return View();
+        }
+
+
+
+
+
+    }
+}
